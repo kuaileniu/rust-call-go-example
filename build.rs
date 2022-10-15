3e80152a826e301001163deb772b00f9e8b5c7a7
jtfeng <jiangtao.feng@baoying.com>
2022-10-15 17:16:28 +0800

    改用python 脚本启动编译go语言

--- build.rs
+++ build.rs
@@ -1,15 +1,20 @@
 use std::process::Command;
-use std::env;
+use std::thread::sleep;
+use std::{env};
+use std::time::Duration;
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
     // 当用go.mod中引用外部库时， 可考虑通过build.rs调用外部python脚本进行打包
     // python中接收 静态文件生成的完整路径
-    Command::new("go").args(&["build", "-o", &format!("{}/libawesome.a", out_dir), "-buildmode=c-archive"])
-        // .arg("src/awesome.go")
-        .arg("awesome.go")
-        .status().unwrap();
+    // Command::new("go").args(&["build", "-o", &format!("{}/libawesome.a", out_dir), "-buildmode=c-archive"])
+    //     .arg("awesome.go")
+    //     .status().unwrap();
 
+    Command::new("python3").arg("gobuild.py").status().unwrap();
+
+    
+    sleep(Duration::from_secs(1));
     println!("cargo:rustc-link-search=native={}", out_dir);
 }
 
--- gobuild.py
+++ gobuild.py
@@ -1,7 +1,12 @@
 # !/usr/bin/env python3
 import os
 
-command = 'go build -o /Volumes/HD/src/rust-learn/rust-call-go-example/target/debug/build/rust-go-example-8b62a236fbe1d0bb/out/libawesome.a -buildmode=c-archive awesome.go'
+command = 'go build -o /Volumes/HD/src/rust-learn/rust-call-go-example/target/debug/build/rust-go-example-8b62a236fbe1d0bb/out/libawesome.a -buildmode=c-archive awesome.go'  # 此行为混合编译后可正确执行行
+# command = 'GOARCH=AMD64 GOOS=darwin go build -o /Volumes/HD/src/rust-learn/rust-call-go-example/target/debug/build/rust-go-example-8b62a236fbe1d0bb/out/libawesome.a -buildmode=c-archive awesome.go'
+# command = 'GOARCH=AMD64 GOOS=linux go build -o /Volumes/HD/src/rust-learn/rust-call-go-example/target/debug/build/rust-go-example-8b62a236fbe1d0bb/out/libawesome.a -buildmode=c-archive awesome.go'
+# command = 'GOARCH=AMD64 GOOS=linux go build -t -o /Volumes/HD/src/rust-learn/rust-call-go-example/target/debug/build/rust-go-example-8b62a236fbe1d0bb/out/libawesome.a -buildmode=c-archive awesome.go'
+# command = 'CGO_ENABLED=0 CC="GCC-12" go build -gcflags=-m  -ldflags "-w -s" -o /Volumes/HD/src/rust-learn/rust-call-go-example/target/debug/build/rust-go-example-8b62a236fbe1d0bb/out/libawesome.a -buildmode=c-archive awesome.go'
+
 tt = os.popen(command)
 print(tt)
 print(type(tt))
