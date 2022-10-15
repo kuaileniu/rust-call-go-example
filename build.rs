b3522c1dd8366dd52bc8cbed87d7982bf7b4852f
jtfeng <jiangtao.feng@baoying.com>
2022-10-15 13:15:08 +0800

    f

--- build.rs
+++ build.rs
@@ -3,7 +3,8 @@ use std::env;
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
-
+    // 当用go.mod中引用外部库时， 可考虑通过build.rs调用外部python脚本进行打包
+    // python中接收 静态文件生成的完整路径
     Command::new("go").args(&["build", "-o", &format!("{}/libawesome.a", out_dir), "-buildmode=c-archive"])
         // .arg("src/awesome.go")
         .arg("awesome.go")
--- feng/de.go
+++ feng/de.go
@@ -2,8 +2,8 @@ package feng
 
 import "fmt"
 // 若import github.com/xxx/xx 等非标准库，cargo run 后编译 静态文件问题
-
+// 可考虑通过build.rs调用外部python脚本进行打包
 
 func MainLog(){
-	fmt.Println("------3--feng---------main.log--引用本项目依赖中的输出")
+	fmt.Println("------4--feng---------main.log--引用本项目依赖中的输出")
 }
